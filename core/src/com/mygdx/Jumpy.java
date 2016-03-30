9ab17ebca66151674946dedf0f06365355d7bb2e
danielliauw <danielliauw@hotmail.co.id>
2016-03-30 18:33:34 +0800

    Added files via upload

--- core/src/com/mygdx/Jumpy.java
+++ core/src/com/mygdx/Jumpy.java
@@ -2,6 +2,7 @@ package com.mygdx;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.JumpyHelper.AssetLoader;
 import com.mygdx.Screens.PlayScreen;
 import com.mygdx.Screens.ScreenEnum;
 import com.mygdx.Screens.ScreenManager;
@@ -24,6 +25,8 @@ public class Jumpy extends Game {
 
 	@Override
 	public void create () {
+		AssetLoader.load();
+
 		ScreenManager.getInstance().initialize(this);
 		ScreenManager.getInstance().showScreen( ScreenEnum.LOGIN );
 	}
